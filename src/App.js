4bd704ca9de8609487b0cd542e6859ea9bfd3beb
ricardolimati <ricardodreamsites@gmail.com>
2020-01-01 18:01:28 -0300

    React JS - Buscando Produtos da API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
-import Header from './components/Header';
+import api from './services/api';
+
 import "./styles.css";
+
+import Header from './components/Header';
+import Main from './pages/main';
+
 
 const App = () => (
   <div className="App">
     <Header />
+    <Main />
   </div>
 );
-    
-    export default App;
+
+export default App;
